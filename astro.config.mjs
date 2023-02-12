518e9d83436019faf24061ea1f791ad9134d5c12
Yuma Kitamura <jiyuujinunite@gmail.com>
2023-02-12 15:45:18 +0900

    Revert "use image component"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
-import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        svelte(),
-        image({
-            serviceEntryPoint: '@astrojs/image/sharp',
-        }),
-    ],
+	integrations: [svelte()],
+	legacy: {
+		astroFlavoredMarkdown: true,
+	},
 })
