78bcf033ac9f514d40b39916cfa1b23eef7ba498
jiyuujin <jiyuujinunite@gmail.com>
2023-11-07 07:13:23 +0900

    chore: astro to 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config'
-import image from '@astrojs/image'
 import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		svelte(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
 	],
 	legacy: {
 		astroFlavoredMarkdown: true,
